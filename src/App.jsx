b46a82880350c8513f54e14cb2b0f930851c3679
Ahmad1267 <ahmadawanking1996@gmail.com>
2026-09-16 16:48:18 +0500

    five

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,2 @@
 
-import { createElement } from 'react'
-import {createRoot} from 'react-dom/client'
 
-const root = createRoot(document.getElementById('root'))
-const reactElement = createElement('h1', null, "Hello React")
-console.log(reactElement)
-root.render(
- < reactElement />
-)
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,10 @@
-// import {createRoot} from 'react-dom/client'
+import {createRoot} from 'react-dom/client'
 
-// const root = createRoot(document.getElementById('root'))
-// root.render(
-//   <ul>
-//     <li>React</li>
-//     <li>React is a library</li>
-//     <li>React is very Famous</li>
-//   </ul>
-// )
+const root = createRoot(document.getElementById('root'))
+root.render(
+  <ul>
+    <li>React</li>
+    <li>React is a library</li>
+    <li>React is very Famous</li>
+  </ul>
+)
