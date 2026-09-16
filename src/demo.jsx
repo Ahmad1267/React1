859e0353b5ae472ca3b68eac5d1e471505ba7b17
Ahmad1267 <ahmadawanking1996@gmail.com>
2026-09-16 19:01:23 +0500

    one

---
+++ src/demo.jsx
@@ -0,0 +1,9 @@
+import {createRoot} from "react-dom/client"
+
+const root = createRoot(document.getElementById("root"))
+root.render(
+    <>
+    <img src="caption.jpg" alt="err"/>
+    <h1>Pretty</h1>
+    </>
+)
